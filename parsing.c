ef84dc46655da81ac3c347419539e7bc0c4268d2
ranlei <ranleixxx@gmail.com>
2014-10-23 15:18:20 +0800

    add s-expression read

--- parsing.c
+++ parsing.c
@@ -20,30 +20,110 @@ void add_history(char* unused){}
 #include <editline/history.h>
 
 #endif
-enum{LERR_DIV_ZERO,LERR_BAD_OP,LERR_BAD_NUM};
-enum{LVAL_NUM,LVAL_ERR};
+enum{LERR_DIV_ZERO, LERR_BAD_OP, LERR_BAD_NUM };
+enum{LVAL_NUM, LVAL_ERR, LVAL_SYM, LVAL_SEXPR };
 
 typedef struct {
     int type;
     long num;
-    int err;
+    char* err;
+    char* sym;
+
+    int count;/*count Pointer to list of "lval*"*/
+    struct lval** cell;
 } lval;
 
 
-lval lval_num(long x){
-    lval v;
-    v.type = LVAL_NUM;
-    v.num = x;
+/*constructor of number*/
+lval* lval_num(long x){
+    lval* v = malloc(sizeof(lval));
+    v->type = LVAL_NUM;
+    v->num = x;
     return v;
 }
 
-
-lval lval_err(int x){
-    lval v;
-    v.type = LVAL_ERR;
-    v.err = x;
+/*constructor of err*/
+lval* lval_err(char* m){
+    lval* v = malloc(sizeof(lval));
+    v->type = LVAL_ERR;
+    v->err = malloc(strlen(m)+1);
+    strcpy(v->err,m);
     return v;
 }
+
+/*constructor of symbol*/
+lval* lval_sym(char* s){
+    lval* v = malloc(sizeof(lval));
+    v->type = LVAL_SYM;
+    v->sym = malloc(strlen(s)+1);
+    strcpy(v->sym,s);
+    return v;
+}
+
+/*constructor of S-expression*/
+lval* lval_sexpr(void){
+    lval* v = malloc(sizeof(lval));
+    v->type = LVAL_SEXPR;
+    v->count = 0;
+    v->cell = NULL;
+    return v;
+
+
+}
+
+/*destructor of lval type.*/
+
+void lval_del(lval* v){
+    switch(v->type){
+        case LVAL_NUM: break;
+        case LVAL_ERR: free(v->err);break;
+        case LVAL_SYM: free(v->sym);break;
+        case LVAL_SEXPR: 
+            for(int i = 0;i<v->count;i++)
+                lval_del(v->cell[i]);
+            free(v->cell);
+        break;
+    }
+
+    free(v);
+}
+
+/*add S-expression*/
+
+lval* lval_add(lval* v, lval* x){
+    v->count++;
+    v->cell = realloc(v->cell, sizeof(lval*) * v->count);
+    v->cell[v->count-1] = x;
+    return v;
+}
+
+lval* lval_read_num(mpc_ast_t* t){
+    errno = 0;
+    long x = strtol(t->contents, NULL, 10);
+    return errno != ERANGE ? lval_num(x) : lval_err("invalid number");    
+}
+
+lval* lval_read(mpc_ast_t* t){
+    if(strstr(t->tag,"number")){return lval_read_num(t);}
+    if(strstr(t->tag,"symbol")){return lval_sym(t->contents);}
+    
+    lval* x = NULL;
+    if(strcmp(t->tag,'>') == 0) {x = lval_sexpr();}
+    if(strstr(t->tag,"sexpr")) {x = lval_sexpr();}
+
+    for (int i = 0;i < t->children_num;i++){
+        if(strcmp(t->children[i]->contents, "(") == 0){continue;}
+        if(strcmp(t->children[i]->contents, ")") == 0){continue;}
+        if(strcmp(t->children[i]->contents, "}") == 0){continue;}
+        if(strcmp(t->children[i]->contents, "{") == 0){continue;}
+        if(strcmp(t->children[i]->tag, "regex") == 0){continue;}
+        x = lval_add(x, lval_read(t->children[i]));
+    }
+
+    return x;
+
+}
+
 
 void lval_print(lval v){
     switch(v.type){
@@ -106,16 +186,19 @@ lval eval(mpc_ast_t* t){
 
 int main(int argc,char** argv){
     mpc_parser_t* Number = mpc_new("number");
-    mpc_parser_t* Operator = mpc_new("operator");
+    mpc_parser_t* Symbol = mpc_new("symbol");
+    mpc_parser_t* Sexpr = mpc_new("sexpr");
     mpc_parser_t* Expr = mpc_new("expr");
     mpc_parser_t* Simplelisp = mpc_new("simplelisp");
     mpca_lang(MPCA_LANG_DEFAULT,
             "\
             number: /-?[0-9]+/ ;\
-            operator: '+' | '-' | '*' |'/' ;\
-            expr: <number> | '('<operator> <expr>+ ')' ;\
-            simplelisp: /^/ <operator> <expr>+ /$/ ;\
-            ",Number,Operator,Expr,Simplelisp);
+            symbol: '+'|'-'|'*'|'/';\
+            Sexpr: '(' <expr>* ')' ;\
+            expr: <number> | <symbol> | <sexpr> ;\
+            simplelisp: /^/ <expr>* /$/ ;\
+            ",
+            Number, Symbol, Sexpr, Expr, Simplelisp);
 
     
     
@@ -153,6 +236,6 @@ int main(int argc,char** argv){
         free(input);
     
     }
-    mpc_cleanup(4,Number,Operator,Expr,Simplelisp);
+    mpc_cleanup(5,Number, Symbol, Sexpr, Expr, Simplelisp);
     return 0;
 }
